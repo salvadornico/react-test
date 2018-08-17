9ff5ea0c1f7230c6b7b8391f2f9d35d3fe910e85
Nico Salvador <nicosalvador@creatinginfo.com>
2018-08-17 15:21:07 +0800

    Up to video #16

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react"
-import Ninjas from "./Ninjas"
+import { Ninjas } from "./Ninjas"
 
 export default class App extends Component {
+	state = {
+		ninjas: [
+			{ id: 1, name: "Ryu", age: 25, belt: "Black" },
+			{ id: 2, name: "Ken", age: 20, belt: "Green" },
+			{ id: 3, name: "Hayabusa", age: 35, belt: "Black" },
+		],
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>My React App</h1>
-				<p>Welcome 🙂</p>
-				<Ninjas />
+				<p>
+					Welcome{" "}
+					<span role="img" aria-label="smiley">
+						🙂
+					</span>
+				</p>
+				<Ninjas ninjas={this.state.ninjas} />
 			</div>
 		)
 	}
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react"
+import React from "react"
 
-export default class Ninjas extends Component {
-	render() {
+export const Ninjas = ({ ninjas }) => {
+	const ninjaList = ninjas.map(ninja => {
+		if (ninja.age <= 20) {
+			return null
+		}
+
 		return (
-			<div className="ninja">
-				<div>Name: Ryu</div>
-				<div>Age: 30</div>
-				<div>Belt: Black</div>
+			<div className="ninja" key={ninja.id}>
+				<div>Name: {ninja.name}</div>
+				<div>Age: {ninja.age}</div>
+				<div>Belt: {ninja.belt}</div>
 			</div>
 		)
-	}
+	})
+
+	return <div className="ninja-list">{ninjaList}</div>
 }
