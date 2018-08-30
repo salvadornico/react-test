613db8d6e3745fda41f6e64c495bf7f437bde9f5
Nico Salvador <nicosalvador@creatinginfo.com>
2018-08-30 15:04:07 +0800

    Basic todo list functionality

---
+++ todo-app/src/App.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+import { Todos } from "./Todos.component"
+import { AddTodo } from "./AddTodo.component"
+
+export class App extends Component {
+	state = {
+		todos: [
+			{ id: 1, content: "Buy milk" },
+			{ id: 2, content: "Play Mario" },
+		],
+	}
+
+	addTodo = todo => {
+		todo.id = Math.random()
+		this.setState({
+			todos: [...this.state.todos, todo],
+		})
+	}
+
+	deleteTodo = id => {
+		this.setState({
+			todos: this.state.todos.filter(todo => todo.id !== id),
+		})
+	}
+
+	render() {
+		return (
+			<div className="todo-app container">
+				<h1 className="center blue-text">Todo's</h1>
+				<Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+				<AddTodo addTodo={this.addTodo} />
+			</div>
+		)
+	}
+}
